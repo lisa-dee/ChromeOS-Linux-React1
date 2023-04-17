831a4c7c2d701c767069532bd416a77f6d678984
Lisa <lisa.salavat@gmail.com>
2023-04-16 17:17:56 -0700

    Spacex with GraphQL fun;

--- chromeos-linux-react1/src/App.js
+++ chromeos-linux-react1/src/App.js
@@ -1,18 +1,69 @@
 import './App.css';
-import { client } from "./ApolloClient/client";
-import { ApolloProvider } from '@apollo/client';
-import ExchangeRatePage from './ExchangeRatePage';
+import Launches from './modules/Launches';
+import Missions from './modules/Missions';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-function App() {
   return (
-  
-    <ApolloProvider client={client}>
-      <div className="App">
-        <ExchangeRatePage />
-      </div>
-    </ApolloProvider>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
   );
 }
 
-export default App;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+export default function App() {
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  }
+
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+
+
+    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="Launches" />
+        <Tab label="Missions" />
+        <Tab label="Rockets" />
+      </Tabs>  
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          <Launches />
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          <Missions />
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          Coming soon
+        </TabPanel>
+    </Box>
+    </div>
+  );
+}
